38ba6b5ee09119c64eb62fe8a005d2d5f98afe71
Yasaratcha <111747429+Yasaratcha@users.noreply.github.com>
2025-08-24 02:18:44 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BiteAndBrew</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+
+    <link rel="stylesheet" href="assets/css/style.css?v=6">
+<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=B7BFCBWz954Q39YSXhFCxZ7WYzvFRl5Pzht6ILrodp17hUqWRIZzVQ5HA_b70TedwQtHv77ovAfE317JV_60hS_ERXPLiYOliTdtx9f0SmUA13-ZHyC-O37NRxcI9xMUmFJoRde4tw1Jcnj9-CMQHS2oNH3PubXWIr_VPQgA_7IhnzE1Tap1Na7nwtBZcKLGBjEl3Vg1XUvQbQu71qyVmrCZ0CCDITdV0IK2JXx6k_Emm3ClhK68CAWZJMw7Bu1JxuF48nPqaNKxlZukfAn0UgY2en1JHipeAAO0ub-DJ5MbbY8cudRIIpijp9FjRWKpw9Xm7wMRcA9VayhByok0NcpfriztGC3ckOP42Lyw6187ZQREVObW8LTRRlAFvkt_PSWocYuEzOAMyESyPM1KhTFH4WgrDQotQgOT_Ld70cLh8EnzPfPpgLY7DZoKtRmIj0pI-krFuh3UkRuCHDCNbUn1s1JXSihfDrk_arcsD1eA4en-tjMNFTkAhrYE82O9ls0Lty-tZmasOdOaDlZqbv8wSRSZAmTs5Xa4WpbvOjYtB5WGaFe61VIWmK9G6MuCm_tufr7G1smojzq4g0gG7oPRFfR60VP9QbQoIDHM3_bloI9Rc3OO3Ix4jZadnh6o_cGZd1qKbdchzW_fuPaTNw" charset="UTF-8"></script></head>
+<body>
+
+
+<!--NavBar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
+        <div class="container">
+          <a href="index.php"><img class="logo1" src="assets/imgs/logo.jpg"></a>
+          <div class="brand text-nowrap mx-1">
+          <h3>Bite And Brews</h3>
+          </div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse nav-button" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="menu.php">Menu</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+
+              <li class="nav-item">
+                <a href="cart.php"><i class="fas fa-shopping-basket"></i></a>
+                <a href="account.php"><i class="fas fa-user"></i></a>
+              </li>
+
+              
+            
+            </ul>
+          </div>
+        </div>
+      </nav>
